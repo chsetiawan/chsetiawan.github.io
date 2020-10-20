0bc6489a83e00510abf564a326d2b2185855d2d4
Christian <>
2020-10-20 11:37:01 -0700

    updated card box shadow, navbar title JS

--- index.js
+++ index.js
@@ -12,3 +12,29 @@ $('.mobile-menu-name').click(() => {
   $('#overlay').toggleClass('open');
   $('#postiontoggle').toggleClass('unset-fixed');
 })
+
+
+
+var didScroll;
+// on scroll, let the interval function know the user has scrolled
+$(window).scroll(function(event){
+  didScroll = true;
+});
+// run hasScrolled() and reset didScroll status
+setInterval(function() {
+  if (didScroll) {
+    // console.log('scrolled');
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+function hasScrolled() {
+  // do stuff here...
+  if (window.scrollY > 400) {
+    console.log("past 500")
+    if ($('#navbar-title').hasClass('visibility-hidden')) {
+      $('#navbar-title').removeClass('visibility-hidden');
+      $('#navbar-title').addClass('second');
+    }
+  }
+}
